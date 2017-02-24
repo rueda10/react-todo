66a00cf3a6fc5c1a0830fa60d70bbb40e940e4ff
Jaime Rueda <rueda10@Jaimes-MacBook-Pro.local>
2017-02-23 20:20:07 -0600

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,6 +18,37 @@ module.exports = {
 
     }
 
-    return $.isArray(todos) ? todos : []; 
+    return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.text.toLowerCase();
+      // filter keeps item if true is returned, discards if false
+      // if no seach text entered, always true
+      // if search text entered, but not in todo's text, false & false
+      // if search text entered, and it is in todo's text, false & true
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
